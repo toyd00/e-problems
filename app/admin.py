ae65b07257d438eacc6959e70c9681503a3bac2c
taishi <taishi.castle@gmail.com>
2021-10-01 10:45:45 +0900

    問題作成が容易になるようクラスを追加

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Subject, Problem, Choice
 
 
+class ChoineInLine(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+class ProblemAdmin(admin.ModelAdmin):
+    inlines = [ChoineInLine]
+
+
 admin.site.register(Subject)
-admin.site.register(Problem)
+admin.site.register(Problem, ProblemAdmin)
 admin.site.register(Choice)
