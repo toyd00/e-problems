10f2a94c274686dc3b64c0fc35a1e6c3f53c08cf
taishi <taishi.castle@gmail.com>
2021-10-01 10:47:29 +0900

    問題の種類と問題作成日追加　その他細かい変更

--- app/models.py
+++ app/models.py
@@ -1,21 +1,31 @@
+import datetime
+
 from django.db import models
+from django.db.models.deletion import PROTECT
+from django.db.models.fields import DateField
 
 class Subject(models.Model):
-    name = models.CharField('科目名', max_length=50)
-    explain = models.TextField('', max_length=400)
+    name = models.CharField("科目名", max_length=50)
+    explain = models.TextField("科目に関する説明", max_length=400)
     
     def __str__(self):
         return self.name
     
+class Type(models.Model):
+    name = models.CharField("種類名", max_length=20, default="基礎")
+
+    def __str__(self):
+        return self.name
 
 
 class Problem(models.Model):
-    title = models.CharField(max_length=20)
-    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+    type = models.ForeignKey(verbose_name="問題の種類", to=Type, max_length=20, on_delete=PROTECT, default=Type(id=1))
+    subject = models.ForeignKey(verbose_name="科目", to=Subject, on_delete=models.PROTECT)
     content = "次のうちから正しいものを１つ選びなさい。"
-    
+    made_date = DateField("問題作成日", default=datetime.date.today)
+
     def __str__(self) -> str:
-        return self.title
+        return self.type + ' ' + self.subject
 
 class Choice(models.Model):
     content = models.TextField()
