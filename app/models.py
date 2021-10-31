12ca2b6e537f15c61b2371da179ec84a7ff984a0
taishi <taishi.castle@gmail.com>
2021-10-31 19:48:48 +0900

    Likeモデルを追加　その他少々の変更

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.db import models
-from django.db.models.deletion import PROTECT
+from django.db.models.deletion import CASCADE, PROTECT
 from django.db.models.fields import DateField
 
 class Subject(models.Model):
@@ -12,25 +12,32 @@ class Subject(models.Model):
         return self.name
 
 class Type(models.Model):
-    name = models.CharField("種類名", max_length=20, default="基礎")
+    name = models.CharField("問題難易度", max_length=20, default="基礎")
 
     def __str__(self):
         return self.name
 
 
+class Like(models.Model):
+    user = models.ManyToManyField(verbose_name="いいねをしたユーザ", to='users.CustomUser')
+    count = models.IntegerField("いいねの数", default=0)
+
 class Problem(models.Model):
-    type = models.ForeignKey(verbose_name="問題の種類", to=Type, max_length=20, on_delete=PROTECT)
+    type = models.ForeignKey(verbose_name="問題の難易度", to=Type, max_length=20, on_delete=PROTECT)
     subject = models.ForeignKey(verbose_name="科目", to=Subject, on_delete=models.PROTECT)
     content = models.TextField("問題文", default="次のうちから正しいものを１つ選びなさい")
     made_date = DateField("問題作成日", default=datetime.date.today)
     correct_choice = models.IntegerField("正解の選択肢", default=1)
+    like = models.OneToOneField(verbose_name="いいね", to=Like, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.type.name + ' ' + self.subject.name
 
 class Choice(models.Model):
-    content = models.TextField()
+    content = models.CharField(max_length=200)
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return "Question:" + self.problem.content + "\nSelect" + self.content
+
+
