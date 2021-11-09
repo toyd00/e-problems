fe6e5e5284eabcca25d28830d20251668f1a2ade
taishi <taishi.castle@gmail.com>
2021-11-09 19:51:47 +0900

    問題作成のフォームを削除できるように変更

--- static/app/js/make_problem.js
+++ static/app/js/make_problem.js
@@ -1,6 +1,7 @@
 {
-  let i = 2;
-  const total_forms = document.getElementById('id_choice_set-TOTAL_FORMS')
+  const total_forms = document.getElementById('id_form-TOTAL_FORMS')
+  const regrex = new RegExp('__prefix__', 'g');
+  const options = document.querySelector('#options');
   document.querySelector('#add').addEventListener('click', (event) => {
     if (event) {
       event.preventDefault();
@@ -9,12 +10,29 @@
     empty_form.setAttribute('class', 'problem-form');
     const empty_form_count = document.getElementsByClassName('problem-form').length
     empty_form.setAttribute('id', `form-${empty_form_count}`);
-    const options = document.querySelector('#options');
-    const regrex = new RegExp('__prefix__', 'g');
     empty_form.innerHTML = empty_form.innerHTML.replace(regrex, empty_form_count)
-    total_forms.setAttribute('value', empty_form_count + 1)
+    total_forms.setAttribute('value', empty_form_count)
     options.append(empty_form);
-
-    i += 1;
+    const button = document.getElementById('remove-button').cloneNode(true);
+    //const button = document.createElement('button');
+    //button.textContent = '削除'
+    button.setAttribute('id', `remove-${empty_form_count}`);
+    button.setAttribute('class', 'remove');
+    button.setAttribute('data-id', `${empty_form_count}`);
+    options.append(button);
   });
+
+}
+{
+  $(document).on('click', '.remove', function(event) {
+    console.log('iiii')
+    if (event) {
+      event.preventDefault();
+    }
+    const id = $(this).data('id');
+    console.log('#form-' + (id - 1));
+    $('#form-' + id).remove();
+    $('#remove-' + id).remove();
+    $('#string').remove();
+  })
 }
