53046661e94665caf73ef8cdf25a2ed88c91ae31
taishi <taishi.castle@gmail.com>
2021-11-18 19:33:50 +0900

    本番環境と開発環境をDEBUGで条件分岐

--- learning/settings.py
+++ learning/settings.py
@@ -115,13 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = ([
+if DEBUG:
+    STATICFILES_DIRS = ([
     os.path.join(BASE_DIR, 'static'),
 ])
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 
